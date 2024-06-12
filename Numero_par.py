412b8d49f97d84a138f647e4b7d52817410de870
JPotrichDeveloper <jpotrichreis@gmail.com>
2024-06-12 15:38:09 -0300

    Add files via upload

---
+++ Numero_par.py
@@ -0,0 +1,9 @@
+import math
+
+num1 = 0
+resultado = 0
+
+num1 = int(input("Informe um numero para verificar se este é par ou impar! "))
+if num1 % 2 == 0:
+    print("Esse número é par! ")
+else: print("Ele é ímpar! ")
