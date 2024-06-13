2766602c23fbf05187c7aa149dfeac3e172ce005
JPotrichDeveloper <jpotrichreis@gmail.com>
2024-06-13 15:16:59 -0300

    Organizado os arquivos

---
+++ Lógica de programação/Maior_numero.py
@@ -0,0 +1,15 @@
+import math
+
+num1 = 0
+num2 = 0
+num3 = 0
+resultado = 0
+
+num1 = int(input("Informe o primeiro numero! "))
+num2 = int(input("Informe o segundo numero! "))
+num3 = int(input("Informe o terceiro numero! "))
+
+if num1 > num2 and num2 > num3:
+    print("Esse número é o maior! ")
+#elif num2 > num3: print("Esse número é o maior! ")
+#elif num3 > num1: print("Esse número é o maior! ")
--- Numero_par.py
+++
@@ -1,9 +0,0 @@
-import math
-
-num1 = 0
-resultado = 0
-
-num1 = int(input("Informe um numero para verificar se este é par ou impar! "))
-if num1 % 2 == 0:
-    print("Esse número é par! ")
-else: print("Ele é ímpar! ")
