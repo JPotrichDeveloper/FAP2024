858033eca54b6f8cf698ae4a0c538d88ee5a9c1e
JPotrichDeveloper <jpotrichreis@gmail.com>
2024-06-17 15:50:59 -0300

    Add files via upload

---
+++ Lógica de programação/Valor_horasTrabalhadas.py
@@ -0,0 +1,8 @@
+horasTrabalho = 0.00
+valor_hora = 0.00
+salario_mes = 0.00
+calculo = 0.00
+
+valor_hora = float(input("Qual é o valor que voce ganha por hora! "))
+horasTrabalho = float(input("Qual é o numero de horas trabalhadas! "))
+
