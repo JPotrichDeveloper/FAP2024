85dbaebc6c7f492405df81d341c3eb61a3e1d254
JPotrichDeveloper <jpotrichreis@gmail.com>
2024-06-18 19:11:28 -0300

    Add files via upload
    Atividades feitas em casa.

---
+++ Lógica de programação/Conversao_Fahrenheit.py
@@ -0,0 +1,12 @@
+import math
+
+Fahrenheit = 0.00
+Celsius = 1.80
+conversao = 0.00
+
+Fahrenheit =float(input("Informe a temperatura em Fahrenheit para conversão! "))
+
+conversao = (Fahrenheit - 32.0) / Celsius
+print("A conversão da temperatura em Fahrenheit para Celsius é! ", + conversao)
+
+
