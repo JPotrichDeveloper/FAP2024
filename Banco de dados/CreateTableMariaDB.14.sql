94f232a3afc27af857f78c6fff7980aec4169e65
JPotrichDeveloper <jpotrichreis@gmail.com>
2024-06-18 19:02:50 -0300

    Rename CreateTableMariaDB.14.sql to Inserir_Membros.sql

--- Banco de dados/CreateTableMariaDB.14.sql
+++
@@ -1,1 +0,0 @@
-insert into Membros (membro_id, nome, cargo, genero) values (4, "Ana Castela", "Programador 4", "F")
---
+++ Banco de dados/Inserir_Membros.sql
@@ -0,0 +1,1 @@
+insert into Membros (membro_id, nome, cargo, genero) values (4, "Ana Castela", "Programador 4", "F")
