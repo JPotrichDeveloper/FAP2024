a5f6119de4e52419dcf4592efa57b4afb87fa7c6
JPotrichDeveloper <jpotrichreis@gmail.com>
2024-06-18 22:14:49 -0300

    Add files via upload
    Atividades feitas em casa no dia 18/06/2024 referente ao dia 17/06/2024.

--- Lógica de programação/Conversao_Fahrenheit.py
+++ Lógica de programação/Conversao_Fahrenheit.py
@@ -7,6 +7,8 @@ conversao = 0.00
 Fahrenheit =float(input("Informe a temperatura em Fahrenheit para conversão! "))
 
 conversao = (Fahrenheit - 32.0) / Celsius
-print("A conversão da temperatura em Fahrenheit para Celsius é! ", + conversao)
+
+print(f"A temperatura em graus Fahrenheit informada corresponde a! {Fahrenheit:.1f}")
+print(f"A conversão da temperatura em Fahrenheit para Celsius é! {conversao:.1f}")
 
 
--- Lógica de programação/Valor_horasTrabalhadas.py
+++ Lógica de programação/Valor_horasTrabalhadas.py
@@ -1,8 +1,12 @@
-horasTrabalho = 0.00
-valor_hora = 0.00
-salario_mes = 0.00
-calculo = 0.00
+import math
 
-valor_hora = float(input("Qual é o valor que voce ganha por hora! "))
-horasTrabalho = float(input("Qual é o numero de horas trabalhadas! "))
+salario_hora = 0.00
+HorasTrabalho = 0.00
+salario_total = 0.00
 
+salario_hora = float(input("Qual é o valor que voce ganha por hora! "))
+HorasTrabalho = float(input("Qual é o numero de horas trabalhadas no mês! "))
+
+salario_total = salario_hora * HorasTrabalho
+
+print(f"O salário mensal a receber é! {salario_total:.2f}")
